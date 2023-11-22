1a2a75782f32cbebaa075296174210bfb1974b95
Samuel609 <samabera03@gmail.com>
2023-11-22 16:07:13 +0300

    sightseeing done

---
+++ app/controllers/sightseeings_controller.rb
@@ -0,0 +1,9 @@
+class SightseeingsController < ApplicationController
+    include Common
+
+    private
+
+    def model_params
+        params.require(:payload).permit(:location, :description, :price_per_hour, :time_spent)
+    end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :vehicles
   resources :drivers
+  resources :sightseeings
+
 
 
 end
