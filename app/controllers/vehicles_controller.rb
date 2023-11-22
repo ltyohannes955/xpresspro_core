831ff26ac73e12251c4fdd472d5c283e7a2c567c
Samuel609 <samabera03@gmail.com>
2023-11-22 12:45:28 +0300

    controllers done

---
+++ app/controllers/vehicles_controller.rb
@@ -0,0 +1,9 @@
+class VehiclesController < ApplicationController
+    include Common
+
+    private
+
+    def model_params
+        params.require(:payload).permit(:vehicle_name, :vehicle_model, :luggage_capacity, :price_per_day, :person_capacity)
+    end 
+end
