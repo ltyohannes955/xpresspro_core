a7943d1ad03fadd908bc1a271dded196e1e8f1d3
Sami609 <152096691+Sami609@users.noreply.github.com>
2023-11-27 19:20:09 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -85,48 +85,6 @@ Journey.create!(
 
 
 
-# Create Sightseeings
-Sightseeing.create!(
-  location: 'City Center',
-  description: 'A bustling city center with shops and restaurants.',
-  price_per_hour: 10,
-  time_spent: 2
-)
-# seed.rb
-
-# ...
-
-# Create Sightseeings
-Sightseeing.create!(
-  location: 'Eiffel Tower',
-  description: 'Iconic iron tower located in Paris, France.',
-  price_per_hour: 15,
-  time_spent: 3
-)
-
-Sightseeing.create!(
-  location: 'Great Wall of China',
-  description: 'World-famous wall in northern China, built to protect against invasions.',
-  price_per_hour: 20,
-  time_spent: 4
-)
-
-Sightseeing.create!(
-  location: 'Sydney Opera House',
-  description: 'Architectural masterpiece and iconic performing arts venue in Sydney, Australia.',
-  price_per_hour: 18,
-  time_spent: 2
-)
-
-Sightseeing.create!(
-  location: 'Machu Picchu',
-  description: 'Ancient Inca citadel situated in the Andes Mountains, Peru.',
-  price_per_hour: 25,
-  time_spent: 5
-)
-
-# ...
-
 
 # Create Seats
 Seat.create!(
