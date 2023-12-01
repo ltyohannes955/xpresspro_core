68e8ad60c0c281c1c76c61f066b6669cf6d1cfb2
Sami609 <152096691+Sami609@users.noreply.github.com>
2023-12-01 16:12:50 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@ Vehicle.create!(
   price_per_day: 50,
   luggage_capacity: 3,
   weightcapacity: 1500,
-  sideview: 'suv1_side.jpg',
-  backview: 'suv1_back.jpg',
-  frontview: 'suv1_front.jpg'
+  sideview: 'https://imgd.aeplcdn.com/370x208/cw/ec/28085/Land-Rover-Range-Rover-Velar-Right-Front-Three-Quarter-92180.jpg?v=201711021421&q=80',
+  backview: 'https://imgd.aeplcdn.com/370x208/cw/ec/28085/Land-Rover-Range-Rover-Velar-left-rear-three-quarter-92182.jpg?v=201711021421&q=80',
+  frontview: 'https://imgd.aeplcdn.com/370x208/cw/ec/28085/Land-Rover-Range-Rover-Velar-Left-Front-Three-Quarter-92181.jpg?v=201711021421&q=80'
 )
 
 Vehicle.create!(
@@ -20,26 +20,114 @@ Vehicle.create!(
   price_per_day: 40,
   luggage_capacity: 2,
   weightcapacity: 1200,
-  sideview: 'sedan1_side.jpg',
-  backview: 'sedan1_back.jpg',
-  frontview: 'sedan1_front.jpg'
+  sideview: 'https://imgd.aeplcdn.com/370x208/n/cw/ec/116201/c-class-exterior-right-side-view-5.jpeg?isig=0&q=80',
+  backview: 'https://imgd.aeplcdn.com/370x208/n/cw/ec/116201/c-class-exterior-right-side-view-2.jpeg?isig=0&q=80',
+  frontview: 'https://imgd.aeplcdn.com/370x208/n/cw/ec/116201/c-class-exterior-front-view-23.jpeg?isig=0&q=80'
+)
+
+Vehicle.create!(
+  vehicle_name: 'Mercedes-Benz',
+  vehicle_model: 'G Wagon',
+  person_capacity: 6,
+  price_per_day: 80,
+  luggage_capacity: 4,
+  weightcapacity: 2100,
+  sideview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/36129/g-class-exterior-left-side-view.jpeg?isig=0&q=80&wm=1',
+  backview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/36129/g-class-exterior-right-side-view.jpeg?isig=0&q=80&wm=1',
+  frontview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/150621/g-class-exterior-front-view.jpeg?isig=0&q=80&wm=1'
+)
+
+Vehicle.create!(
+  vehicle_name: 'Mercedes-Benz',
+  vehicle_model: 'Maybac',
+  person_capacity: 4,
+  price_per_day: 70,
+  luggage_capacity: 3,
+  weightcapacity: 1500,
+  sideview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/94041/amg-a35-limousine-exterior-left-side-view-2.jpeg?q=80&wm=1',
+  backview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/94041/amg-a35-limousine-exterior-right-side-view-2.jpeg?q=80&wm=1',
+  frontview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/94041/amg-a35-limousine-exterior-front-view-2.jpeg?q=80&wm=1'
+)
+
+Vehicle.create!(
+  vehicle_name: 'Mahindra',
+  vehicle_model: 'Thar',
+  person_capacity: 5,
+  price_per_day: 65,
+  luggage_capacity: 3,
+  weightcapacity: 1500,
+  sideview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/40087/thar-exterior-left-rear-three-quarter.jpeg?q=80&wm=1',
+  backview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/40087/thar-exterior-right-side-view.jpeg?q=80&wm=1',
+  frontview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/40087/thar-exterior-front-view.jpeg?q=80&wm=1'
+)
+
+Vehicle.create!(
+  vehicle_name: 'Toyota',
+  vehicle_model: 'Rav 4',
+  person_capacity: 6,
+  price_per_day: 55,
+  luggage_capacity: 5,
+  weightcapacity: 1900,
+  sideview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/124027/urban-cruiser-hyryder-exterior-left-side-view-2.jpeg?isig=0&q=80&wm=1',
+  backview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/124027/urban-cruiser-hyryder-exterior-right-side-view-2.jpeg?isig=0&q=80&wm=1',
+  frontview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/124027/urban-cruiser-hyryder-exterior-right-front-three-quarter-71.jpeg?isig=0&q=80&wm=1'
+)
+
+Vehicle.create!(
+  vehicle_name: 'BMW',
+  vehicle_model: 'QX-18',
+  person_capacity: 6,
+  price_per_day: 75,
+  luggage_capacity: 6,
+  weightcapacity: 1800,
+  sideview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/140591/new-x1-exterior-left-side-view.jpeg?isig=0&q=80&wm=1',
+  backview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/140591/new-x1-exterior-right-side-view.jpeg?isig=0&q=80&wm=1',
+  frontview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/140591/new-x1-exterior-right-front-three-quarter-6.jpeg?isig=0&q=80&wm=1'
+)
+
+Vehicle.create!(
+  vehicle_name: 'Renault',
+  vehicle_model: 'Suv',
+  person_capacity: 5,
+  price_per_day: 35,
+  luggage_capacity: 4,
+  weightcapacity: 1900,
+  sideview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/141857/kiger-exterior-left-side-view.jpeg?isig=0&q=80&wm=1',
+  backview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/141857/kiger-exterior-right-side-view-2.jpeg?isig=0&q=80&wm=1',
+  frontview: 'https://imgd.aeplcdn.com/1056x594/n/cw/ec/141857/kiger-exterior-right-front-three-quarter-5.jpeg?isig=0&q=80&wm=1'
 )
 
 # Sightseeings
 Sightseeing.create!(
-  location: 'Museum of Art',
+  location: 'Berlin City',
   description: 'A beautiful museum showcasing various art collections.',
-  price_per_hour: 10,
+  price_per_hour: 30,
   time_spent: 2,
-  image: 'museum_image.jpg'
+  image: 'https://images.unsplash.com/photo-1467269204594-9661b134dd2b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 )
 
 Sightseeing.create!(
-  location: 'City Park',
+  location: 'Aifel Tower',
   description: 'A large park with scenic views and recreational activities.',
-  price_per_hour: 5,
+  price_per_hour: 30,
   time_spent: 1,
-  image: 'park_image.jpg'
+  image: 'https://media.istockphoto.com/id/1436430810/photo/paris-eiffel-tower.jpg?s=612x612&w=0&k=20&c=twnPoKvqoTpraJNYTNrLBKASWjExRQMxoY9XPkHWJFQ='
+)
+
+Sightseeing.create!(
+  location: 'Napels',
+  description: 'A large park with scenic views and recreational activities.',
+  price_per_hour: 40,
+  time_spent: 1,
+  image: 'https://images.unsplash.com/photo-1507840771025-26e8ececa04c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+)
+
+Sightseeing.create!(
+  location: 'Budapest',
+  description: 'A large park with scenic views and recreational activities.',
+  price_per_hour: 20,
+  time_spent: 1,
+  image: 'https://images.unsplash.com/photo-1500078974918-738828bc0422?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 )
 
 # Seats
