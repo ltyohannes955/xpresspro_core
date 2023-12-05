18f1f9c2e31382101ae3c0e349015f30ab313316
Leul Yohannes <106162490+ltyohannes955@users.noreply.github.com>
2023-12-05 12:42:24 +0300

    a few updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,21 @@
 class UsersController < ApplicationController
     def create
-        @user = User.create(user_params)
+        user = User.create!(user_params)
 
-        if @user.valid?
-            token = encode_token({ user_id: @user.id })
-            render json: { user: @user, token: token, status: "ok"}, status: :ok
+        if user.valid?
+            token = encode_token({ user_id: user.id })
+            render json: { user: user, token: token, status: "ok"}, status: :ok
         else
             render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
         end
     end
 
     def login
-        @user =  User.find_by(email: user_params[:email])
+        user =  User.find_by(email: user_params[:email])
 
-        if @user && @user.authenticate(user_params[:password])
-            token = encode_token({ user_id: @user.id })
-            render json: { user: @user, token: token, status: "ok" }, status: :ok
+        if user && user.authenticate(user_params[:password])
+            token = encode_token({ user_id: user.id })
+            render json: { user: user, token: token, status: "ok" }, status: :ok
         else
             render json: { error: 'Invalid username or password', status: "unprocessable_entity" }, status: :unprocessable_entity
         end
