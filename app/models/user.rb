15c4b281e613418e88c119f5982c9a5c4dc35cee
Leul Yohannes <106162490+ltyohannes955@users.noreply.github.com>
2023-12-05 13:21:02 +0300

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :first_name, :last_name, :email, :phone_number, :password_digest, :password_confirmation, presence: true
+    validates :first_name, :last_name, :email, :phone_number, :password, :password_confirmation, presence: true
     validates :email, uniqueness: true
 end
