0b173624f770521ba7194a48b7a62da7c4a175ec
Leul Yohannes <106162490+ltyohannes955@users.noreply.github.com>
2023-12-06 03:42:08 +0300

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
     def create
         user = User.create!(user_params)
 
+        user.password = params[:user][:password]
+
         if user.valid?
             token = encode_token({ user_id: user.id })
             render json: { user: user, token: token, status: "ok"}, status: :ok
