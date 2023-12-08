829ebce463a91d83b3686ecc6396d352f1fab6c2
Samuel Abera <samabera03@gmail.com>
2023-12-08 09:58:18 +0300

    some refactoring

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -1,7 +1,13 @@
 class Api::NotificationsController < ApplicationController
   def create
-    user = User.find(params[:user_id])
-    NotificationMailer.send_notification(user).deliver_now
-    render json: { message: 'Notification email sent' }
+    begin
+      user = User.find(params[:user_id])
+      NotificationMailer.send_notification(user).deliver_now
+      render json: { message: 'Notification email sent' }
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: "User not found with ID #{params[:user_id]}" }, status: :not_found
+    rescue StandardError => e
+      render json: { error: "An error occurred: #{e.message}" }, status: :internal_server_error
+    end
   end
 end
