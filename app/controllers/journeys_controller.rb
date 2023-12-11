03662458c579394e24d0759785051df8219b74d9
Sami609 <152096691+Sami609@users.noreply.github.com>
2023-12-11 17:52:24 +0300

    Update journeys_controller.rb

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -3,6 +3,20 @@ class JourneysController < ApplicationController
 
   private
   def model_params
-    params.require(:payload).permit(:origin, :destination, :Departure_Date, :Departure_time, :status, :user_id, :driver_id, :vehicle_id, list_of_Sightseeing: [])
+    permitted_attributes = params.require(:payload).permit(
+      :origin, 
+      :destination, 
+      :Departure_Date, 
+      :Departure_time, 
+      :status, 
+      :user_id, 
+      :driver_id, 
+      :vehicle_id
+    )
+  
+    permitted_attributes[:list_of_Sightseeing] =
+      params.dig(:payload, :list_of_Sightseeing)&.map { |sightseeing| sightseeing.permit! }
+  
+    permitted_attributes
   end
 end
