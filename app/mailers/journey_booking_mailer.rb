b067b8d1a04fae1ce28487bdf4b0a4516813d378
Sami609 <152096691+Sami609@users.noreply.github.com>
2023-12-13 00:21:08 +0300

    Update journey_booking_mailer.rb

--- app/mailers/journey_booking_mailer.rb
+++ app/mailers/journey_booking_mailer.rb
@@ -2,10 +2,9 @@
 class JourneyBookingMailer < ApplicationMailer
     default from: "xpresspro.service@gmail.com"
 
-    def booking_confirmation(user, journey)
-      @user = user
-      @journey = journey
-      mail( to: @user.email, subject: 'Journey Booking Confirmation')
+    def booking_confirmation(recipient_email)
+      mail( to: recipient_email , subject: 'Journey Booking Confirmation')
     end
   end
+
   
